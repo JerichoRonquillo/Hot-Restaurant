a6dadd91590fbbfc2edac22c7b7609c96ffcb615
ckitazumi <codykitazumi@gmail.com>
2021-08-25 21:00:22 -0700

    pathing

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 //require express
 const express = require("express");
+const path = require('path');
 const app = express();
 //define port
 const PORT = 3000;
 
+//routing
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, 'food.html')));
+
+app.get('/waitList', (req, res) => res.sendFile(path.join(__dirname, 'waitList.html')));
+app.get('/makeRes', (req, res) => res.sendFile(path.join(__dirname, 'makeRes.html')));
+
 //listen
 app.listen(PORT, () => {
-    console.log(`Server is listening at: https://localhost:${PORT}/`)
+    console.log(`Server is listening at: http://localhost:${PORT}/`)
 })
 
 let waitingList = [
@@ -27,4 +35,9 @@ let currentReservations = [
         email: "Kevin@Kevin.Kevin",
         phone: "1238675309"
     }
-]
+];
+
+
+
+
+
